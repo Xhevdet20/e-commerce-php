c8163c3a80998f8ea8ea6892a59f3eae4f567869
xhevdet <xhevdet.kryeziu@kujtesa.com>
2020-07-22 18:17:20 +0200

    get_product func part 2

--- resources/functions.php
+++ resources/functions.php
@@ -32,8 +32,27 @@ function get_products() {
   $query = query("SELECT * FROM products");
   confirm($query);
   while($row = fetch_array($query)) {
-    echo $row['product_price'];
-  }
+    
+$product = <<<DELIMETER
+
+<div class="col-sm-4 col-lg-4 col-md-4">
+  <div class="thumbnail">
+    <a href="item.php?id={$row['product_id']}"><img src="{$row['product_image']}" alt=""></a>
+    <div class="caption">
+      <h4 class="pull-right">&#36;{$row['product_price']}</h4>
+      <h4><a href="product.html">{$row['product_title']}</a>
+      </h4>
+      <p>See more snippets like this online store item at <a target="_blank" href="http://www.bootsnipp.com">Bootsnipp - http://bootsnipp.com</a>.</p>
+      <a class="btn btn-primary" target="_blank" href="item.php?id={$row['product_id']}">Add to Cart</a>
+    </div> 
+  </div>
+</div>
+
+DELIMETER;
+
+echo $product;
+
+}
 
 }
 
