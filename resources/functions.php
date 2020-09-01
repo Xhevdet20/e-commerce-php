a8e52d18b24334950c8e01489a0c57e92175c909
xhevdet <xhevdet.kryeziu@kujtesa.com>
2020-09-01 14:05:43 +0200

    cart sessions

--- resources/functions.php
+++ resources/functions.php
@@ -2,6 +2,22 @@
 
 // Helper functions
 
+function set_message($msg){
+  if(!empty($msg)){
+    $_SESSION['message'] = $msg;
+  } else {
+    $msg = "";
+  }
+}
+
+function display_message(){
+  if(isset($_SESSION['message'])){
+    echo $_SESSION['message'];
+    unset($_SESSION['message']);
+  }
+}
+
+
 function redirect($location) {
   header("Location: $location");
 }
@@ -27,6 +43,10 @@ function fetch_array($result){
   return mysqli_fetch_array($result);
 } 
 
+/******************************************************
+ * FRONT END FUNCTIONS 
+ */
+
 // get Products
 function get_products() {
   $query = query("SELECT * FROM products");
@@ -40,20 +60,140 @@ $product = <<<DELIMETER
     <a href="item.php?id={$row['product_id']}"><img src="{$row['product_image']}" alt=""></a>
     <div class="caption">
       <h4 class="pull-right">&#36;{$row['product_price']}</h4>
-      <h4><a href="product.html">{$row['product_title']}</a>
+      <h4><a href="item.php?id={$row['product_id']}">{$row['product_title']}</a>
       </h4>
       <p>See more snippets like this online store item at <a target="_blank" href="http://www.bootsnipp.com">Bootsnipp - http://bootsnipp.com</a>.</p>
-      <a class="btn btn-primary" target="_blank" href="item.php?id={$row['product_id']}">Add to Cart</a>
+      <a class="btn btn-primary" target="_blank" href="cart.php?add={$row['product_id']}">Add to Cart</a>
     </div> 
   </div>
-</div>
+</div> 
 
 DELIMETER;
 
 echo $product;
 
-}
+  }
 
 }
+
+
+// get Products in cat page
+function get_products_in_cat_page() {
+  $query = query("SELECT * FROM products WHERE product_category_id =". escape_string($_GET['id']) . " ");
+  confirm($query);
+  while($row = fetch_array($query)) {
+    
+$product = <<<DELIMETER
+
+<div class="col-md-3 col-sm-6 hero-feature">
+<div class="thumbnail">
+    <img src="{$row['product_image']}" alt="">
+    <div class="caption">
+        <h3>{$row['product_title']}</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+        <p>
+            <a href="#" class="btn btn-primary">Buy Now!</a> <a href="item.php?id={$row['product_id']}" class="btn btn-default">More Info</a>
+        </p>
+    </div>
+</div>
+</div>  
+
+DELIMETER;
+echo $product;
+  }
+}
+
+// get Products in shop page
+function get_products_in_shop_page() {
+  $query = query("SELECT * FROM products ");
+  confirm($query);
+  while($row = fetch_array($query)) {
+    
+$product = <<<DELIMETER
+
+<div class="col-md-3 col-sm-6 hero-feature">
+<div class="thumbnail">
+    <img src="{$row['product_image']}" alt="">
+    <div class="caption">
+        <h3>{$row['product_title']}</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+        <p>
+            <a href="#" class="btn btn-primary">Buy Now!</a> <a href="item.php?id={$row['product_id']}" class="btn btn-default">More Info</a>
+        </p>
+    </div>
+</div>
+</div>  
+
+DELIMETER;
+
+echo $product;
+
+  }
+
+}
+
+function get_categories(){
+  $query = query("SELECT * FROM categories");
+  confirm($query);
+
+    while($row = fetch_array($query)) {
+
+      $categories_links = <<<DELIMETER
+
+      <a href='category.php?id={$row['cat_id']}' class='list-group-item'>{$row['cat_title']}</a>
+
+DELIMETER;
+
+      echo $categories_links;
+    }
+}
+
+function login_user(){
+
+  if(isset($_POST['submit'])){
+    $username =  escape_string($_POST['username']);
+    $password =  escape_string($_POST['password']);
+
+  $query = query("SELECT * from users where username = '{$username}' AND password = '{$password}' ");
+  confirm($query);
+
+  if(mysqli_num_rows($query) == 0){
+    set_message("Your Password or Username are wrong");
+    redirect("login.php");
+  } else {
+    set_message("Welcome to Admin {$username}");
+    redirect("admin");
+  }
+
+  }
+
+}
+
+function send_message(){
+  if(isset($_POST['submit'])){
+
+
+    $to = "xhevdet1996@gmail.com";
+    $from_name = $_POST['name'];
+    $subject = $_POST['subject'];
+    $message = $_POST['message'];
+    $email = $_POST['email'];
+
+    $headers = "From: {$from_name} {$email}";
+
+    $result = mail($to, $subject, $message, $headers );
+
+    if(!$result){
+      echo "Error";
+    } else {
+      echo "Sent";
+    }
+  }
+}
+
+
+/******************************************************
+ * BACK END FUNCTIONS 
+ */
 
 ?>
